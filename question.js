4f50da78a0ad79d0541c174bdf1ccb0ddb2b4baf
LindsayMF <meneslindsay@hotmail.com>
2019-11-12 14:01:47 -0700

    Pseudocode written and css updated

--- question.js
+++ question.js
@@ -9,5 +9,30 @@ var questions = [
       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
       answer: "parentheses"
     },
-    ///etc.
+    {
+      title: "In which HTML element do we put the JavaScript?",
+      choices: ["<javascript>", "<scripting>", "<js>", "<script>"],
+      answer: "<script>"
+    },
+    {
+      title: "How do you write 'Hello World' in an alert box?",
+      choices: ["alert('Hello World');", "alertBox('Hello World');", "msg('Hello World');", "msgBox('Hello World');"],
+      answer: "alert('Hello World');"
+    },
+    {
+      title: "How does a FOR loop start?",
+      choices: ["for (i = 0; i <= 5; i++)", "for (i = 0; i <= 5)", "for i = 1 to 5", "for (i <= 5; i++)"],
+      answer: "for (i = 0; i <= 5; i++)"
+    },
+    {
+      title: "An array needs to be enclosed in ____________.",
+      choices: ["{}", "()", "[]", "{()}"],
+      answer: "[]"
+    },
+    {
+      title: "How does a FOR loop start?",
+      choices: ["for (i = 0; i <= 5; i++)", "for (i = 0; i <= 5)", "for i = 1 to 5", "for (i <= 5; i++)"],
+      answer: "for (i = 0; i <= 5; i++)"
+    }
+
   ];
--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+//1. create a call to action 
+    //need a landing page and a start quiz button
+    //when start quiz button hide button
+    //add Back button
+    //****Questions start here****/
 
+
+
+//2. Countdown timer
+    //must start at 0. 
+    //on click of start quiz button, countdown timer starts at 75seconds and counts down to 0.
+    //timer stops at 0 or when the questions are finished.
+    //wrong answer subtracts 10 seconds from remaining time. 
+
+
+
+
+//3. Questions - place js question file before other js file
+    //create submit button and back button
+    //Question 1 appears on start quiz button click
+    //flash correct or wrong on the submit button click
+    //new question appears after answer is displayed correct or wrong (maybe 500ms after submit click??)
+    //stores answer as correct or wrong
+    //quiz ends on submit button click of last question. 
+    //display score page after submit buttom is clicked - have a finish button
+
+//4. High Scores stored in browser (can be separate html file)
+    //prompt user for initials after score page on finish button click
+    //text input box for initials
+    //button to submit initials with score
+    //display (new) page with high scores on submit button click
+    //have button to clear high scores on this page 
+    //have button to restart quiz on this page too 
