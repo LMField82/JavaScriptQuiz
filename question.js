f1b6a4dbddeccd9b690da774d923af5904c01146
LindsayMF <meneslindsay@hotmail.com>
2019-11-16 21:41:31 -0700

    functional application

--- question.js
+++ question.js
@@ -1,52 +1,35 @@
 var questions = [
-    {
-      question: "Commonly used data types DO NOT include:",
-      choiceA: "strings", 
-      choiceB: "booleans", 
-      choiceC: "alerts", 
-      choiceD: "numbers",
-      correct: "choiceC",
-    },
-    {
-      question: "The condition in an if / else statement is enclosed within ____.",
-      choiceA: "quotes", 
-      choiceB: "curly brackets", 
-      choiceC: "parentheses", 
-      choiceD: "square brackets",
-      correct: "choiceC",
-    },
-    {
-      question: "In which HTML element do we put the JavaScript?",
-      choiceA: "<javascript>", 
-      choiceB: "<scripting>", 
-      choiceC: "<js>", 
-      choiceD: "<script>",
-      correct: "choiceD",
-    },
-    {
-      question: "How do you write 'Hello World' in an alert box?",
-      choiceA: "alert('Hello World');",
-      choiceB: "msg('Hello World');", 
-      choiceC: "msgBox('Hello World');", 
-      choiceD: "alertBox('Hello World');", 
-      correct: "choiceA",
-    },
-    {
-      question: "How does a FOR loop start?",
-      choiceA: "for (i = 0; i <= 5; i++)",
-      choiceB: "for (i = 0; i <= 5)",
-      choiceC: "for i = 1 to 5", 
-      choiceD: "for (i <= 5; i++)", 
-      correct: "choiceA",
-    },
-    {
-      question: "An array needs to be enclosed in ____________.",
-      choiceA: "{}",
-      choiceB: "[]",
-      choiceC: "()",
-      choiceD: "{()}", 
-      correct: "choiceB",
-    },
-  ];
+  "Commonly used data types DO NOT include:",
+  "The condition in an if / else statement is enclosed within ____.",
+  "In which HTML element do we put the JavaScript?",
+  "How do you write 'Hello World' in an alert box?",
+  "How does a FOR loop start?",
+  "An array needs to be enclosed in ____________."
+];
 
-  
+var choiceArray = [
+  {
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    correct: "alerts"
+  },
+  {
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    correct: "parentheses",
+  },
+  {
+    choices: ["<javascript>", "<scripting>", "<js>", "<script>"],
+    correct: "<script>",
+  },
+  {
+    choices: ["alert('Hello World');", "msg('Hello World');", "msgBox('Hello World');", "alertBox('Hello World');"],
+    correct: "alert('Hello World');",
+  },
+  {
+    choices: ["for (var i = 0; i <= 5; i++)", "for (var i = 0; i <= 5)", "for i = 1 to 5", "for (i <= 5; i++)"],
+    correct: "for (var i = 0; i <= 5; i++)",
+  },
+  {
+    choices: ["{}", "[]", "()", "{()}"],
+    correct: "[]"
+  }
+];
--- script.js
+++ script.js
@@ -1,100 +1,142 @@
 //Making a JS Quiz
 
 //1. Landing page with start button
-
-//2. On click of start button, start button hides
-document.getElementById("startQuiz").addEventListener("click", function() {
-
-   var disappearButton = document.getElementById("startQuiz");
-   if (disappearButton.style.display === "block") {
-    disappearButton.style.display = "none";
-   }
-   var disappearWelcome = document.getElementById("welcome");
-   if (disappearWelcome.style.display === "block") {
-       disappearWelcome.style.display = "none";
-   }
-
-    //On click of start button, timer starts    
-    var counterDisplay = document.getElementsById("counter");
-    if (counterDisplay.style.display === "none") {
-        counterDisplay.style.display = "block";
+document.addEventListener("DOMContentLoaded", function() {
+  //2. On click of start button, start button hides
+  document.getElementById("startQuiz").addEventListener("click", function() {
+    var disappearButton = document.getElementById("startQuiz");
+    if (disappearButton.style.display === "block") {
+      disappearButton.style.display = "none";
     }
-    var timer = 90000;
-    var countdown = setInterval(function () {
-        timer--;
-        console.log("Timer: " + timer);
-    }, 1000); 
-    //On click of start button, quiz starts
-    document.getElementById("quiz").
-    document.getElementsByClassName("answerButtons").style.display = "block";
+    var disappearWelcome = document.getElementById("welcome");
+    if (disappearWelcome.style.display === "block") {
+      disappearWelcome.style.display = "none";
+    }
+//On click of start button, timer starts
+    var count = 0;
+    var timer = 90; 
+    var counterDisplay = document.getElementById("counter");
+    if (counterDisplay.style.display === "none") {
+      counterDisplay.style.display = "block";
+    }
 
+    var countdown = setInterval(function() {
+      timer--;
+      document.getElementById("counter").innerHTML = timer + "s";
+
+      if (timer === 0) {
+        clearInterval(countdown);
+      }
+    }, 1000);
+
+    var endQuiz = function() {
+      clearInterval(countdown)
+      console.log("QUIZ OVER")
+      var highScore = localStorage.getItem("highScore");
+      var addText = "";
+      if (timer > highScore) {
+        addText = "You got the highest score!";
+        localStorage.setItem('highScore', timer)
+      }
+      document.getElementById("result").innerHTML = `Your final score is: ${timer}. ${addText}`;
+      document.getElementById("highScore").style.display = "block";
+      document.getElementById('score').addEventListener('click', function(e){
+        e.preventDefault()
+       var playerName = document.getElementById('typeName').value;
+       localStorage.setItem(playerName, timer);
+    });
+    
+};
+    var correct = function() {
+      document.getElementById("result").innerHTML = "correct";
+      count++;
+      if (count === questions.length) {
+        endQuiz();
+      } else {
+        displayQuestions();
+      }
+    };
+    var incorrect = function() {
+      document.getElementById("result").innerHTML = "incorrect";
+      count++;
+      timer -= 10;
+      if (count === questions.length) {
+        endQuiz();
+      } else {
+        displayQuestions();
+      }
+    };
+
+    var displayQuestions = function() {
+      var node = document.createElement("h3");
+      node.innerHTML = questions[count];
+      document.getElementById("quiz").innerHTML = "";
+      document.getElementById("answerChoiceBox").innerHTML = "";
+      document.getElementById("quiz").appendChild(node);
+      choiceArray[count].choices.map((choice, i) => {
+        var ansButton = document.createElement("button");
+        var text = document.createTextNode(choice);
+        ansButton.setAttribute("data", choice);
+        ansButton.setAttribute("answer", choiceArray[count].correct);
+        ansButton.setAttribute("id", `btn${i}`);
+        ansButton.appendChild(text);
+        document.getElementById("answerChoiceBox").appendChild(ansButton);
+        document
+          .querySelector(`#btn${i}`)
+          .addEventListener("click", function(event) {
+            if (event.target.getAttribute("data") === choiceArray[count].correct) {
+              correct();
+            } else {
+              incorrect();
+            }
+          });
+      });
+    };
+    displayQuestions();
+
+    //On click of start button, quiz starts
+    // var showQuestion = document.getElementById("quiz");
+    // if (showQuestion.style.display === "none") {
+    //     showQuestion.style.display = "block";
+    // }
+
+    /*var test = document.getElementsByClassName("answerButtons");*/
+    // document.getElementById("choiceA").style.display = "block";
+    // document.getElementById("choiceB").style.display = "block";
+    // document.getElementById("choiceC").style.display = "block";
+    // document.getElementById("choiceD").style.display = "block";
+  });
+
+  //3. Quiz
+
+  //question 1 appears (can be a p, h2, etc elements)
+
+  //answers for Q1 appear as buttons
+
+  //on click of answer, display correct/incorrect
+
+  //if incorrect, timer subtracts 10seconds -- need a loop to cycle through questions in this same manner
+
+  //on click of answer, question 2 appears, repeat above (loop) until last question.
+
+  //for final question, on click of answer stop timer || if time = 0 (before final question)
+
+  //do high score
+
+  //4. High score
+
+  //get initials as an input and tie to score(timer) --- score = amount of time left after answering all question/ score = 0 if timer runs out
+
+  //need enter button for submitting initials with score
+
+  //save to local storage
+
+  //go to high score page (link)
+
+  //pull data from local storage to display on HS page
 });
 
-//3. Quiz
-
-    //question 1 appears (can be a p, h2, etc elements)
-    
-    //answers for Q1 appear as buttons
-
-    //on click of answer, display correct/incorrect
-        
-        //if incorrect, timer subtracts 10seconds -- need a loop to cycle through questions in this same manner
-    
-    //on click of answer, question 2 appears, repeat above (loop) until last question.
-
-    //for final question, on click of answer stop timer || if time = 0 (before final question)
-
-        //do high score
-
-//4. High score
-
-    //get initials as an input and tie to score(timer) --- score = amount of time left after answering all question/ score = 0 if timer runs out
-
-    //need enter button for submitting initials with score
-
-    //save to local storage
-
-    //go to high score page (link) 
-
-    //pull data from local storage to display on HS page
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//1. create a call to action 
+//1. create a call to action
 // var choiceA = document.getElementById("A");
 // var choiceB = document.getElementById("B");
 // var choiceC = document.getElementById("C");
@@ -105,36 +147,31 @@ document.getElementById("startQuiz").addEventListener("click", function() {
 //     //add listener to start quiz button
 //     start.addEventListener("click", startQuiz);
 
-    //when start quiz button is clicked hide button - captain planet ex
+//when start quiz button is clicked hide button - captain planet ex
 
-    //add  submit and Back button
+//add  submit and Back button
 
-    //****Questions start here****/
-
-
+//****Questions start here****/
 
 //2. Countdown timer
-    //must start at 0. 
-    //on click of start quiz button, countdown timer starts at 75seconds and counts down to 0.
-    //timer stops at 0 or when the questions are finished.
-    //wrong answer subtracts 10 seconds from remaining time. 
-
-
-
+//must start at 0.
+//on click of start quiz button, countdown timer starts at 75seconds and counts down to 0.
+//timer stops at 0 or when the questions are finished.
+//wrong answer subtracts 10 seconds from remaining time.
 
 //3. Questions - place js question file before other js file
-    //Question 1 appears on start quiz button click
-    //flash correct or wrong on the submit button click
-    //new question appears after answer is displayed correct or wrong (maybe 500ms after submit click??)
-    //if else to show wrong/right after click submit button
-    //stores answer as correct or wrong
-    //quiz ends on submit button click of last question. 
-    //display score page after submit buttom is clicked - have a finish button
+//Question 1 appears on start quiz button click
+//flash correct or wrong on the submit button click
+//new question appears after answer is displayed correct or wrong (maybe 500ms after submit click??)
+//if else to show wrong/right after click submit button
+//stores answer as correct or wrong
+//quiz ends on submit button click of last question.
+//display score page after submit buttom is clicked - have a finish button
 
 //4. High Scores stored in browser (can be separate html file)
-    //prompt user for initials after score page on finish button click
-    //text input box for initials
-    //button to submit initials with score
-    //display (new) page with high scores on submit button click
-    //have button to clear high scores on this page 
-    //have button to restart quiz on this page too 
+//prompt user for initials after score page on finish button click
+//text input box for initials
+//button to submit initials with score
+//display (new) page with high scores on submit button click
+//have button to clear high scores on this page
+//have button to restart quiz on this page too
